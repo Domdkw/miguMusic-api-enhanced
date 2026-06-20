3e9ea34952bb176596e5d29098bc8bcf30913ade
Domdkw <domdkw@qq.com>
2026-06-20 22:40:32 +0800

    fix: edgeone pages deploy

--- functions/[[default]].ts
+++ functions/[[default]].ts
@@ -3,6 +3,8 @@
  * [[default]].ts 是 EdgeOne 的默认路由配置文件
  * 用于处理所有未被其他函数匹配的请求
  */
-import app from '../src/adapters/edgeone';
+import app from '../src/app';
 
-export default app;
+export async function onRequest(context: any) {
+    return app.fetch(context.request, context.env, context.executionCtx);
+}
