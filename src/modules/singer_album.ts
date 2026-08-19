75e80c1c4bc0908d3b4b124ce2307573789c62e1
Domdkw <Domdkw@qq.com>
2026-08-19 16:44:34 +0800

    fix(singer-album): 修正歌手专辑列表的分页判断逻辑

--- src/modules/singer_album.ts
+++ src/modules/singer_album.ts
@@ -2,6 +2,6 @@ import { h5fetch } from '../utils/h5fetch';
 
 export const getSingerAlbum = async (singerId: string, page: number = 1) => {
     const res = await h5fetch(`http://app.c.nf.migu.cn/bmw/singer/album/v1.0?singerId=${singerId}&pageNo=${page}`);
-    res?.data?.contents.length < 10 ? res.data.hasNext = false : res.data.hasNext = true;
+    res?.data?.header?.nextPageUrl ? res.data.header.hasNext = true : res.data.header.hasNext = false;
     return res;
 };
